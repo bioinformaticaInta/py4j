ce99c2cda2c0b8f4538a69d749ae7a1ded6eb91c
bioinformaticaInta <gonzalez.sergio@inta.gob.ar>
2023-11-16 16:14:38 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from py4j.functions import *
+from py4j.Graph import Graph
+from py4j.SubGraph import SubGraph
+from py4j.Match import Match
+from py4j.DataType import DataType
+from py4j.DataType import Node
+from py4j.DataType import Relationship
